468fe57be692fc7640160f578b75317e7bffc080
OlhaMal <olia23423425@gmail.com>
2023-01-17 19:51:40 +0200

    prettier

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainer = document.querySelector(".gallery");
@@ -7,17 +7,17 @@ const imagesMarkup = createGalleryImagesMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
 function createGalleryImagesMarkup(items) {
-    return items
-      .map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>`;
-      })
-      .join("");
-  }
+    })
+    .join("");
+}
 
-  const lightBox = new SimpleLightbox('.gallery a', { 
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-  });
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
